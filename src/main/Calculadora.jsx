20f9faaafde68ab4c8ce77fe97350db01af7c543
Paulo <pauloh16.pdr@gmail.com>
2020-08-29 17:15:16 -0300

    Adicionar componente Calculadora

---
+++ src/main/Calculadora.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Calculadora.css";
+
+export default class Calculadora extends Component {
+
+
+    render() {
+        return (
+            <div className="Calculadora">
+
+            </div>
+        );
+    }
+}
