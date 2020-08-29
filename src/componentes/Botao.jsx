7fccb230f03d8984f996fba7643a2a2f629eb408
Paulo <pauloh16.pdr@gmail.com>
2020-08-29 18:43:31 -0300

    Ajustar componentes par aplicar logica

--- src/componentes/Botao.jsx
+++ src/componentes/Botao.jsx
@@ -1,4 +1,8 @@
 import React from "react";
 import "./Botao.css";
 
-export default (props) => (<button className={`Botao ${props["classe"]}`}>{props["legenda"]}</button>);
+export default (props) => (
+    <button className={`Botao ${props["classe"]}`} onClick={(e) => props["click"] && props.click(props["legenda"])}>
+        {props["legenda"]}
+    </button>
+);
--- src/main/Calculadora.jsx
+++ src/main/Calculadora.jsx
@@ -4,28 +4,47 @@ import Display from "../componentes/Display";
 import "./Calculadora.css";
 
 export default class Calculadora extends Component {
+    constructor(props) {
+        super(props);
+
+        this.limparMemoria = this.limparMemoria.bind(this);
+        this.setOperacao = this.setOperacao.bind(this);
+        this.addDigito = this.addDigito.bind(this);
+    }
+
+    limparMemoria() {
+        console.log("Limpou!")
+    }
+
+    addDigito(valor) {
+        console.log("Você adicionou!", valor);
+    }
+
+    setOperacao(valor) {
+        console.log("Você operou!", valor);
+    }
 
     render() {
         return (
             <div className="Calculadora">
                 <Display valor="100" />
-                <Botao legenda="AC" classe="AC" />
-                <Botao legenda="/" />
-                <Botao legenda="7" />
-                <Botao legenda="8" />
-                <Botao legenda="9" />
-                <Botao legenda="*" />
-                <Botao legenda="4" />
-                <Botao legenda="5" />
-                <Botao legenda="6" />
-                <Botao legenda="-" />
-                <Botao legenda="1" />
-                <Botao legenda="2" />
-                <Botao legenda="3" />
-                <Botao legenda="+" />
-                <Botao legenda="0" classe="B0" />
-                <Botao legenda="." />
-                <Botao legenda="=" />
+                <Botao classe="triplo" legenda="AC" click={this.limparMemoria} />
+                <Botao classe="operacao" legenda="/" click={this.setOperacao} />
+                <Botao legenda="7" click={this.addDigito} />
+                <Botao legenda="8" click={this.addDigito} />
+                <Botao legenda="9" click={this.addDigito} />
+                <Botao classe="operacao" legenda="*" click={this.setOperacao} />
+                <Botao legenda="4" click={this.addDigito} />
+                <Botao legenda="5" click={this.addDigito} />
+                <Botao legenda="6" click={this.addDigito} />
+                <Botao classe="operacao" legenda="-" click={this.setOperacao} />
+                <Botao legenda="1" click={this.addDigito} />
+                <Botao legenda="2" click={this.addDigito} />
+                <Botao legenda="3" click={this.addDigito} />
+                <Botao classe="operacao" legenda="+" click={this.setOperacao} />
+                <Botao classe="duplo" legenda="0"  click={this.addDigito} />
+                <Botao legenda="." click={this.addDigito} />
+                <Botao classe="operacao" legenda="=" click={this.setOperacao} />
             </div>
         );
     }
